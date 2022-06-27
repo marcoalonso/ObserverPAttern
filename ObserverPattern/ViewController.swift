ea1647d65050b388ca6b16e9b3847f16ba0f3567
Marco Alonso <marco@garagecoders.net>
2022-06-27 16:22:41 -0500

    Empezando

---
+++ ObserverPattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObserverPattern
+//
+//  Created by marco rodriguez on 27/06/22.
+// Ejemplo de primer commit
+
+import UIKit
+import Combine //Este framework permite la programacion reactiva
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
