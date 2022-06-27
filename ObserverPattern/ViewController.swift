268ffd2ff0b2385748112be9698169e3e1de6fad
Marco Alonso <marco@garagecoders.net>
2022-06-27 16:42:22 -0500

    Subscripcion
    agregada

--- ObserverPattern/ViewController.swift
+++ ObserverPattern/ViewController.swift
@@ -10,11 +10,23 @@ import Combine //Este framework permite la programacion reactiva
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    var viewModel = ViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        subscriptions()
     }
 
+    //Crear una suscripcion
+    func subscriptions(){
+        viewModel.title.sink { _ in
+            
+        } receiveValue: { title in
+            //Reaccionar a los cambios
+        }
+
+    }
 
 }
 //Dentro vamos a crear el subject o observable
